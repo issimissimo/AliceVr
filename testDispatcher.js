f52808657a17c83e05586f21451f0ca78890617c
DSuppo <d.suppo@issimissimo.com>
2020-09-30 21:19:22 +0200

    inizio

--- testDispatcher.js
+++ testDispatcher.js
@@ -1,16 +1,82 @@
-window.addEventListener("message", function (event) {
+let iframes = [];
+let iframe = {
+    name: "",
+    loaded: false,
+};
 
-    console.log("received: " + event.data);
+let allIframesLoaded = false;
+
+
+/// collect iframes
+if (typeof window.frames.testReceiver1 !== "undefined") {
+    iframe.name = "testReceiver1";
+    iframes.push(iframe);
+}
+if (typeof window.frames.testReceiver2 !== "undefined") {
+    iframe.name = "testReceiver2";
+    iframes.push(iframe);
+}
+if (typeof window.frames.testReceiver3 !== "undefined") {
+    iframe.name = "testReceiver3";
+    iframes.push(iframe);
+}
+
+
+
+
+/////////////////////////////////////////////////////
+/// send 1st message to iframes to send the source
+/////////////////////////////////////////////////////
+setTimeout(() => {
+
+    for (let i in iframes) {
+        window.frames[i].postMessage("hello", "*");
+    }
+
+}, 1000)
+
+
+
+/// on received message
+window.addEventListener("message", function (event) {
+    console.log("-------------------");
+    console.log("sono Main e ho ricevuto:");
+    console.log(event.data);
+    console.log("-------------------");
+
+    // ///send message to all iframes
+    // for (let i in iframes) {
+    //     window.frames[i].postMessage(event.data, "*");
+    // }
 
 });
 
-setTimeout(function () {
-
-    console.log("MANDO....")
-    let win = window.frames.testReceiver1;
-    win.postMessage("message", "*");
 
 
 
 
-}, 1000)
+
+
+
+// var iframeWindow = window.frames.testReceiver1;
+var iframeElement = document.getElementsByName("testReceiver1")[0];
+
+
+iframeElement.addEventListener("load", function () {
+    console.log("testReceiver1 - loaded")
+});
+
+
+
+    // setTimeout(() => {
+
+    //     if (typeof iframeWindow !== "undefined") {
+
+    //         let data = "paperino"
+    //         // iframeWindow.postMessage(data, "*");
+    //         window.frames[0].postMessage(data, "*");
+
+    //     }
+
+
+    // }, 1000)
